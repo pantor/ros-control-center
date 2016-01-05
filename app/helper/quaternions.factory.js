ab8bcf046798da738fbbb8b8d8d217da557f01ef
Lars <lbersch@gwdg.de>
2016-01-05 12:56:33 +0100

    check if input is defined

--- app/helper/quaternions.factory.js
+++ app/helper/quaternions.factory.js
@@ -1,15 +1,27 @@
 function QuaternionsFactory() {
   return {
     getRoll: function (q) {
-      var rad = Math.atan2(2 * (q.w * q.x + q.y * q.z), 1 - 2 * (q.x * q.x + q.y * q.y));
+      var rad;
+      if (!q) {
+        return '';
+      }
+      rad = Math.atan2(2 * (q.w * q.x + q.y * q.z), 1 - 2 * (q.x * q.x + q.y * q.y));
       return 180 / Math.PI * rad;
     },
     getPitch: function (q) {
-      var rad = Math.asin(2 * (q.w * q.y - q.z * q.x));
+      var rad;
+      if (!q) {
+        return '';
+      }
+      rad = Math.asin(2 * (q.w * q.y - q.z * q.x));
       return 180 / Math.PI * rad;
     },
     getYaw: function (q) {
-      var rad = Math.atan2(2 * (q.w * q.z + q.x * q.y), 1 - 2 * (q.y * q.y + q.z * q.z));
+      var rad;
+      if (!q) {
+        return '';
+      }
+      rad = Math.atan2(2 * (q.w * q.z + q.x * q.y), 1 - 2 * (q.y * q.y + q.z * q.z));
       return 180 / Math.PI * rad;
     },
     getInit: function () {

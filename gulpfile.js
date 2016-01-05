bbb10a544ffcc216024ee66df03ebd4612587754
Lars <lbersch@gwdg.de>
2016-01-05 12:28:23 +0100

    added gulp for concat and linting javascript files

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  eslint = require('gulp-eslint');
+
+// Concat all js files into roscc.js
+gulp.task('js', function () {
+  return gulp.src(['app/app.js', 'app/**/*.js'])
+    .pipe(concat('roscc.js'))
+    .pipe(gulp.dest('assets/js/'));
+});
+
+// Lint javascript based on airbnb ES5 linter and angular code style guide
+gulp.task('js-lint', function() {
+  return gulp.src(['app/**/*.js'])
+    .pipe(eslint({
+      global: ['_', 'ros', 'ROSLIB'],
+      extends: ['airbnb/legacy', 'angular'],
+      rules: { 'vars-on-top': 1 },
+      envs: ['browser'],
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+// Changes will be detected automatically
+gulp.task('watch', function () {
+  gulp.watch('app/**/*.js', ['js']);
+});
+
+gulp.task('default', ['js-lint', 'watch']);
---
+++ node_modules/eslint-config-angular/gulpfile.babel.js
@@ -0,0 +1,43 @@
+'use strict';
+import babelCompiler from 'babel';
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+import istanbul from 'gulp-istanbul';
+import jscs from 'gulp-jscs';
+import jshint from 'gulp-jshint';
+import mocha from 'gulp-mocha';
+
+const configFiles = './gulpfile.babel.js'
+  , srcFiles = 'index.js'
+  , testFiles = 'test/*.js';
+
+gulp.task('lint', () => {
+  return gulp.src([configFiles, srcFiles, testFiles])
+    .pipe(eslint())
+    .pipe(eslint.formatEach('./node_modules/eslint-path-formatter'))
+    .pipe(eslint.failOnError())
+    .pipe(jscs({
+      esnext: true
+    }))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('build', ['lint']);
+
+gulp.task('test', ['build'], (cb) => {
+  gulp.src(['index.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', () => {
+      gulp.src([testFiles])
+        .pipe(mocha({
+          compilers: {
+            js: babelCompiler
+          }
+        }))
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
+});
